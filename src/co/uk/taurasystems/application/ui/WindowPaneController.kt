40d8a0d1e12c0eeeb3b730556200054804d638d8
tauraamui <aplewis95@gmail.com>
2016-08-20 23:50:30 +0100

    updated the date output format

--- src/co/uk/taurasystems/application/ui/WindowPaneController.kt
+++ src/co/uk/taurasystems/application/ui/WindowPaneController.kt
@@ -10,6 +10,7 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.net.URL
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -34,8 +35,20 @@ class WindowPaneController {
 
     private val keysAndValues = HashMap<String, String?>()
 
+    private val oxhDocsFolder = File("oxh_docs")
+    private val oxhDocsOutputFolder = File("oxh_docs\\output")
+                                                //0     1     2     3     4     5     6     7     8     9
+    private var daysAndSuffixes = listOf<String>("th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th",
+                                                        //10    11    12    13    14    15    16    17    18    19
+                                                         "th", "th", "th", "th", "th", "th", "th", "th", "th", "th",
+                                                        //20    21    22    23    24    25    26    27    28    29
+                                                         "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th",
+                                                        //30    31
+                                                         "th", "st")
+
     fun initialize() {
-        okButton?.setOnAction {e -> setupFileMap()
+        okButton?.setOnAction {e ->
+            setupFileMap()
             setupDataMap()
             createOutput()
         }
@@ -43,17 +56,15 @@ class WindowPaneController {
     }
 
     private fun setupFileMap() {
-        val oxhDocsFolder = File("oxh_docs")
-        if (oxhDocsFolder.exists() && oxhDocsFolder.isDirectory) {
-            Dochelper.mapFiles(File("oxh_docs"))
-        } else {
-            oxhDocsFolder.mkdir()
-        }
+        if (!oxhDocsFolder.exists() || !oxhDocsFolder.isDirectory) oxhDocsFolder.mkdir()
+        if (!oxhDocsOutputFolder.exists() || !oxhDocsOutputFolder.isDirectory) oxhDocsOutputFolder.mkdir()
+        Dochelper.mapFiles(oxhDocsFolder)
     }
 
     private fun setupDataMap() {
         keysAndValues.put("{ref}", refTextField?.text!!)
-        keysAndValues.put("{date}", if (letterDatePicker?.value == null) "" else letterDatePicker?.value.toString())
+        keysAndValues.put("{date}", getDatePickerValue(letterDatePicker))
+        //keysAndValues.put("{date}", if (letterDatePicker?.value == null) "" else letterDatePicker?.value.toString())
         keysAndValues.put("{title}", titleTextField?.text!!)
         keysAndValues.put("{patient_full_name}", patientFullNameTextField?.text!!)
         keysAndValues.put("{address_line_1}", addressLine1TextField?.text!!)
@@ -62,20 +73,53 @@ class WindowPaneController {
         keysAndValues.put("{address_line_4}", addressLine4TextField?.text!!)
         keysAndValues.put("{post_code}", postCodeTextField?.text!!)
         keysAndValues.put("{patient_abbreviated_name}", patientAbbrNameTextField?.text!!)
-        keysAndValues.put("{appointment_date_time}", (if (appointmentDatePicker?.value == null) "" else appointmentDatePicker?.value.toString()) + " " + (appointmentTimeTextField?.text!!))
+        //keysAndValues.put("{appointment_date_time}", getDatePickerValue(appointmentDatePicker) + " " + appointmentTimeTextField?.text!!)
+        getDatePickerValue(datePicker = letterDatePicker)
+        getDatePickerValue(datePicker = appointmentDatePicker)
+        keysAndValues.put("{appointment_date_time}", "${getDatePickerValue(appointmentDatePicker)} ${appointmentTimeTextField?.text}")
+        //keysAndValues.put("{appointment_date_time}", (if (appointmentDatePicker?.value == null) "" else appointmentDatePicker?.value.toString()) + " " + (appointmentTimeTextField?.text!!))
 
         for ((key, value) in keysAndValues) {
             println(key + " " + value)
         }
     }
 
+    private fun getDatePickerValue(datePicker: DatePicker?): String {
+        //var formattedDate: StringBuilder = StringBuilder()
+        val calendar = Calendar.getInstance()
+
+        if (datePicker == null || datePicker.value == null) return ""
+
+        val day = datePicker.value.dayOfMonth
+        val month = datePicker.value.month.toString()
+        val year = datePicker.value.year
+
+        val formattedDate = "$day${daysAndSuffixes[day]} ${camelCase(month)} $year"
+
+        return formattedDate.toString()
+    }
+
     private fun createOutput() {
         for ((fileTypeName, file) in Dochelper.filePathAndDocType) {
             val editedDoc = Dochelper.findAndReplaceKeysInFile(file, keysAndValues)
             if (fileTypeName.equals("unknown")) continue
-            val outputStream = FileOutputStream("oxh_docs\\${patientFullNameTextField?.text} $fileTypeName.doc")
+            val outputStream = FileOutputStream("$oxhDocsOutputFolder\\${patientFullNameTextField?.text} $fileTypeName.doc")
             editedDoc?.write(outputStream)
             outputStream.close()
         }
+    }
+
+    private fun camelCase(stringToReformat: String): String {
+        var stringLowerCase = stringToReformat.toLowerCase()
+        val stringBuilder = StringBuilder()
+        stringBuilder.setLength(stringLowerCase.length)
+        stringLowerCase.forEachIndexed { i, c ->
+            if (i == 0) {
+                stringBuilder.set(i, stringLowerCase[i].toUpperCase())
+            } else {
+                stringBuilder.set(i, stringLowerCase[i])
+            }
+        }
+        return stringBuilder.toString()
     }
 }
